5d13b158e6de09156dde48ab47ef7f87bb71726d
fh.proger <phillipe.souza@rte.com.br>
2019-12-19 18:40:23 -0300

    Alteração de teste

--- ProductApi/Controllers/WeatherForecastController.cs
+++ ProductApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ProductApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+           "SuperTeste", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
